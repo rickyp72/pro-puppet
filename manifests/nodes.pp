8ef6c71de63d92dacf11c0339ff7d3197adc7107
Ricky Parkinson <ricky@Rickys-MacBook-Pro.local>
2013-09-24 07:15:52 +0100

    new commit

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,13 @@
-node 'redhat.local' {
-  
+node 'rhel6.propuppet.local' {
+  include base
+
+}
+
+class base {
+  #include ssh
+  #include postfix
+  #include mysql
+  package {'git':
+    ensure => present,
+  }
 }
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,1 @@
-include nodes
+import 'nodes.pp'
