8bb8d5c7f6c9ac55d551b84f472edc9518a23b32
Ricky Parkinson <ricky@Rickys-MacBook-Pro.local>
2013-09-24 07:52:15 +0100

    vhost declared

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,6 +1,13 @@
 node 'rhel6.propuppet.local' {
   include base
-
+  include apache
+  apache::vhost {'www.example.com':
+    port => 8888,
+    docroot => '/var/www/html/www.example.com',
+    ssl => false,
+    priority => 10,
+    serveraliases => 'live.example.com'
+  }
 }
 
 class base {
